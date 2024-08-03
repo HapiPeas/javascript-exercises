2734df86eff956c36e654da59aa7ce7059056090
Benson Li <94872538+HapiPeas@users.noreply.github.com>
2024-08-03 16:53:45 -0400

    feat: Finish javascript exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(oldArray, ...args) {
+    let newArray = [];
+
+    for (let i = 0; i < oldArray.length; i++) {
+        let flag = false;
+        for (let j = 0; j < args.length; j++) {
+            if (oldArray.at(i) === args.at(j)) {
+                flag = true;
+            }
+        }
+        if (flag == false) {
+            newArray.push(oldArray.at(i));
+        }
+    }
+
+    return newArray;
 };
 
 // Do not edit below this line
