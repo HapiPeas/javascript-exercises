47a47581a2c0758bb5d150ea0d91ff17f6b68d1d
Benson Li <94872538+HapiPeas@users.noreply.github.com>
2024-08-03 17:14:28 -0400

    feat: Finish javascript exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+    if (startNum < 0 || endNum < 0) {
+        return "ERROR";
+    }
+    if (Number.isInteger(startNum + endNum) == false) {
+        return "ERROR";
+    }
 
+    let max = 0 , min = 0;
+    if (startNum > endNum) {
+        max = startNum;
+        min = endNum;
+    }
+    else {
+        max = endNum;
+        min = startNum;
+    }
+
+    let terms = max - min + 1;
+    const difference = 1;
+
+    let sum = (terms / 2) * (2 * min + (terms - 1) * difference);
+
+    return sum;
 };
 
 // Do not edit below this line
