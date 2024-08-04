5eea683e24ece0f7d695a318698b797a9e76f474
Benson Li <94872538+HapiPeas@users.noreply.github.com>
2024-08-03 21:52:16 -0400

    feat: Finish javascript exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemp) {
+  let celsius = (5 / 9) * (fahrenheitTemp - 32);
+
+  return Number(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  let fahrenheit = ((9 / 5) * celsiusTemp + 32);
+
+  return Number(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
